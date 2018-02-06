044009564c62eb2136da0ac87f78eccef58b357c
srivastavaaviral1 <srivastavaaviral1@gmail.com>
2018-02-06 17:33:55 +0530

    added properties

---
+++ js/custom.js
@@ -0,0 +1,70 @@
+(function ($) {
+
+  "use strict";
+
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
+    
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $('.top-nav-collapse .container .navbar-header a img').attr('src','./images/logo(119).png')
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+            $('.navbar-fixed-top .container .navbar-header a img').attr('src','./images/logo(119)_white.png')
+          }
+    });
+    
+
+    // PARALLAX EFFECT
+    $.stellar({
+      horizontalScrolling: false,
+    }); 
+
+
+    // MAGNIFIC POPUP
+    $('.image-popup').magnificPopup({
+        type: 'image',
+        removalDelay: 300,
+        mainClass: 'mfp-with-zoom',
+        gallery:{
+          enabled:true
+        },
+        zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+        // openerElement is the element on which popup was initialized, in this case its <a> tag
+        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
+
+
+    // SMOOTH SCROLL
+    $(function() {
+      $('.custom-navbar a, #home a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
+
+})(jQuery);
